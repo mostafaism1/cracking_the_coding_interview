96d1df05dd131e36eb6a7abe5724d37231b958e9
mostafa <me.cssign@gmail.com>
2021-06-09 21:04:39 +0200

    Add tests for IsUnique2

---
+++ src/test/java/org/mostafaism/problemsolving/ArraysAndStrings/IsUnique/IsUnique2Test.java
@@ -0,0 +1,27 @@
+package org.mostafaism.problemsolving.ArraysAndStrings.IsUnique;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+
+public class IsUnique2Test {
+
+    @Test
+    void it_should_return_true_on_a_unique_string() {
+        // given
+        IsUnique isUnique = new IsUnique();
+
+        // when, then
+        then(isUnique.isUnique2("abc")).isTrue();
+    }
+
+    @Test
+    void it_should_return_false_on_a_non_unique_string() {
+        // given
+        IsUnique isUnique = new IsUnique();
+
+        // when, then
+        then(isUnique.isUnique2("abca")).isFalse();
+    }
+
+}
