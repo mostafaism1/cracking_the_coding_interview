b64096f53e4a274f013c209d7dd9ef586defbbf1
mostafa <me.cssign@gmail.com>
2021-06-09 23:59:20 +0200

    Add tests for CheckPermutation

---
+++ src/test/java/org/mostafaism/problemsolving/ArraysAndStrings/CheckPermutation/CheckPermutation2Test.java
@@ -0,0 +1,36 @@
+package org.mostafaism.problemsolving.ArraysAndStrings.CheckPermutation;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckPermutation2Test {
+
+    @Test
+    void it_should_return_true() {
+        // given
+        CheckPermutation checkPermutation = new CheckPermutation();
+
+        // when, then
+        then(checkPermutation.checkPermutation2("abc", "bac")).isTrue();
+    }
+
+    @Test
+    void it_should_return_false() {
+        // given
+        CheckPermutation checkPermutation = new CheckPermutation();
+
+        // when, then
+        then(checkPermutation.checkPermutation2("abc", "abd")).isFalse();
+    }
+
+    @Test
+    void it_should_return_false_when_the_strings_have_different_lengths() {
+        // given
+        CheckPermutation checkPermutation = new CheckPermutation();
+
+        // when, then
+        then(checkPermutation.checkPermutation2("abc", "abcd")).isFalse();
+    }
+
+}
