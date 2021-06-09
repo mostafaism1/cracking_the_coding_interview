fa5d4e62184d7104dffdfcc1a3d16908090b8cab
mostafa <me.cssign@gmail.com>
2021-06-09 02:33:19 +0200

    Define ArrayList interface

---
+++ src/main/java/org/mostafaism/problemsolving/datastructure/ArrayList/ArrayList.java
@@ -0,0 +1,9 @@
+package org.mostafaism.problemsolving.datastructure.ArrayList;
+
+public interface ArrayList<E> {
+    void push(E element);
+
+    void set(int index, E value);
+
+    E get(int index);
+}
