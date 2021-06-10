70c2606514929913b43b9ed4fd0a07242d6586cb
mostafa <me.cssign@gmail.com>
2021-06-10 12:49:33 +0200

    Refactor IsUnique towards a more functional style using Streams

--- src/main/java/org/mostafaism/problemsolving/ArraysAndStrings/IsUnique/IsUnique.java
+++ src/main/java/org/mostafaism/problemsolving/ArraysAndStrings/IsUnique/IsUnique.java
@@ -1,39 +1,29 @@
 package org.mostafaism.problemsolving.ArraysAndStrings.IsUnique;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 public class IsUnique {
 
-    public boolean isUnique1(String string) {
-        Set<Character> set = new HashSet<>();
-        for (Character character : string.toCharArray()) {
-            if (set.contains(character)) {
-                return false;
-            }
-            set.add(character);
-        }
-        return true;
+    public boolean isUnique1(String str) {
+        return str.chars().distinct().count() == str.length() ? true : false;
     }
 
-    public boolean isUnique2(String string) {
-        String sorted = sort(string);
-        char previous = string.charAt(0);
-        for (int i = 1; i < sorted.length(); i++) {
-            char c = sorted.charAt(i);
-            if (c == previous) {
-                return false;
-            }
-            previous = c;
-        }
-        return true;
+    public boolean isUnique2(String str) {
+        String sortedStr = sort(str);
+        return hasDifferentSubsequentCharacters(sortedStr) ? true : false;
     }
 
-    private String sort(String string) {
-        char[] chars = string.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
+    // FIXME Replace with an in-place sorting algorithm.
+    private String sort(String str) {
+        return str.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    private boolean hasDifferentSubsequentCharacters(String str) {
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
