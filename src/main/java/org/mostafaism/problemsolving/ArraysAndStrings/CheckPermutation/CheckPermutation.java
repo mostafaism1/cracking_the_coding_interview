a46b16287b6f01882c5d066e761738b6d0b95f0a
mostafa <me.cssign@gmail.com>
2021-06-10 12:49:49 +0200

    Refactor CheckPermutation towards a more functional style using Streams

--- src/main/java/org/mostafaism/problemsolving/ArraysAndStrings/CheckPermutation/CheckPermutation.java
+++ src/main/java/org/mostafaism/problemsolving/ArraysAndStrings/CheckPermutation/CheckPermutation.java
@@ -1,8 +1,10 @@
 package org.mostafaism.problemsolving.ArraysAndStrings.CheckPermutation;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CheckPermutation {
 
@@ -10,65 +12,38 @@ public class CheckPermutation {
         if (str1.length() != str2.length()) {
             return false;
         }
-        Map<Character, Integer> characterFrequancy = getCharacterFrequancy(str1);
-        if (!decrementFrequancy(characterFrequancy, str2)) {
-            return false;
-        }
-        if (!isZero(characterFrequancy)) {
-            return false;
-        }
-        return true;
+        Map<Character, Integer> characterFrequancy1 = getCharacterFrequancy(str1);
+        Map<Character, Integer> characterFrequancy2 = getCharacterFrequancy(str2);
+        Map<Character, Integer> differenceFrequancy = substractMaps(characterFrequancy1, characterFrequancy2);
+        return allZero(differenceFrequancy) ? true : false;
     }
 
     public boolean checkPermutation2(String str1, String str2) {
         if (str1.length() != str2.length()) {
             return false;
         }
-        String sortedStr1 = sortString(str1);
-        String sortedStr2 = sortString(str2);
-        for (int i = 0; i < str1.length(); i++) {
-            if (sortedStr1.charAt(i) != sortedStr2.charAt(i)) {
-                return false;
-            }
-        }
-        return true;
+        String sortedStr1 = sort(str1);
+        String sortedStr2 = sort(str2);
+        return sortedStr1.equals(sortedStr2) ? true : false;
     }
 
-    private Map<Character, Integer> getCharacterFrequancy(String str1) {
-        Map<Character, Integer> characterFrequancy = new HashMap<>();
-        for (Character c : str1.toCharArray()) {
-            if (characterFrequancy.containsKey(c)) {
-                characterFrequancy.put(c, characterFrequancy.get(c) + 1);
-            } else {
-                characterFrequancy.put(c, 1);
-            }
-        }
-        return characterFrequancy;
+    private Map<Character, Integer> getCharacterFrequancy(String str) {
+        return str.chars().mapToObj(i -> (char) i)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
     }
 
-    private boolean decrementFrequancy(Map<Character, Integer> characterFrequancy, String str) {
-        for (Character c : str.toCharArray()) {
-            if (characterFrequancy.containsKey(c)) {
-                characterFrequancy.put(c, characterFrequancy.get(c) - 1);
-            } else {
-                return false;
-            }
-        }
-        return true;
+    private Map<Character, Integer> substractMaps(Map<Character, Integer> map1, Map<Character, Integer> map2) {
+        return Stream.of(map1, map2).flatMap(m -> m.entrySet().stream())
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, Math::subtractExact));
     }
 
-    private boolean isZero(Map<Character, Integer> characterFrequancy) {
-        for (Integer count : characterFrequancy.values()) {
-            if (count != 0) {
-                return false;
-            }
-        }
-        return true;
+    private boolean allZero(Map<Character, Integer> map) {
+        return map.values().stream().anyMatch(v -> v != 0) ? false : true;
     }
 
-    private String sortString(String str) {
-        char[] chars = str.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
+    // FIXME Replace with an in-place sorting algorithm.
+    private String sort(String str) {
+        return str.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
