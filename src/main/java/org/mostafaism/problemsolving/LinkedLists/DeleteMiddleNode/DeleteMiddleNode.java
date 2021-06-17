39346789b8cfa8d6dbbbd0a2fe7e5996b3b56003
mostafa <me.cssign@gmail.com>
2021-06-17 23:27:53 +0200

    Add javadoc comment

--- src/main/java/org/mostafaism/problemsolving/LinkedLists/DeleteMiddleNode/DeleteMiddleNode.java
+++ src/main/java/org/mostafaism/problemsolving/LinkedLists/DeleteMiddleNode/DeleteMiddleNode.java
@@ -4,6 +4,12 @@ import org.mostafaism.problemsolving.datastructure.LinkedList.Node;
 
 public class DeleteMiddleNode<K> {
 
+    /**
+     * Deletes node from its list.
+     * 
+     * @param node the node to be deleted; must be a middle node, with a node before
+     *             and a node after
+     */
     public void deleteMiddleNode(Node<K> node) {
         node.key = node.next.key;
         node.next = node.next.next;
