1878c7211cc292026a5c637781b731e65e8cfd2b
mostafa <me.cssign@gmail.com>
2021-06-21 19:57:04 +0200

    Implement MyQueue

---
+++ src/main/java/org/mostafaism/problemsolving/StacksAndQueues/QueueViaStacks/MyQueue.java
@@ -0,0 +1,54 @@
+package org.mostafaism.problemsolving.StacksAndQueues.QueueViaStacks;
+
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+
+import org.mostafaism.problemsolving.datastructure.Queue.Queue;
+import org.mostafaism.problemsolving.datastructure.Stack.DefaultStack;
+import org.mostafaism.problemsolving.datastructure.Stack.Stack;
+
+public class MyQueue<E> implements Queue<E> {
+
+    private Stack<E> stack;
+    private Stack<E> temp;
+
+    public MyQueue() {
+        stack = new DefaultStack<>();
+        temp = new DefaultStack<>();
+    }
+
+    @Override
+    public void add(E item) {
+        while (!stack.isEmpty()) {
+            temp.push(stack.pop());
+        }
+        stack.push(item);
+        while (!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+    }
+
+    @Override
+    public E remove() {
+        try {
+            return stack.pop();
+        } catch (EmptyStackException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public E peek() {
+        try {
+            return stack.peek();
+        } catch (EmptyStackException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+}
