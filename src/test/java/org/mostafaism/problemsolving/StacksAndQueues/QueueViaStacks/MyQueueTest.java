7b1d4f0fcea23b18d934414369fd0c49a017900e
mostafa <me.cssign@gmail.com>
2021-06-21 19:56:52 +0200

    Add tests for MyQueue

---
+++ src/test/java/org/mostafaism/problemsolving/StacksAndQueues/QueueViaStacks/MyQueueTest.java
@@ -0,0 +1,12 @@
+package org.mostafaism.problemsolving.StacksAndQueues.QueueViaStacks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mostafaism.problemsolving.datastructure.Queue.QueueTest;
+
+public class MyQueueTest extends QueueTest {
+
+    @BeforeEach
+    void setup() {
+        queue = new MyQueue<>();
+    }
+}
