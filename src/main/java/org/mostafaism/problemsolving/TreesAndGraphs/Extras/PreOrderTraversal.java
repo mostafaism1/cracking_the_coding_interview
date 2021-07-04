5b1fe7b610a3bc8867910faa35e1da513eba74bb
mostafa <me.cssign@gmail.com>
2021-07-04 15:08:10 +0200

    Implement PreOrderTraversal

---
+++ src/main/java/org/mostafaism/problemsolving/TreesAndGraphs/Extras/PreOrderTraversal.java
@@ -0,0 +1,27 @@
+package org.mostafaism.problemsolving.TreesAndGraphs.Extras;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.mostafaism.problemsolving.datastructure.Tree.BinaryTreeNode;
+
+public class PreOrderTraversal<E> {
+
+    public List<E> preOrderTraversal(BinaryTreeNode<E> root) {
+        // Base case.
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        // Recursive step.
+        List<E> left = preOrderTraversal(root.left());
+        List<E> right = preOrderTraversal(root.right());
+        List<E> result = new ArrayList<>();
+        result.add(root.data());
+        result.addAll(left);
+        result.addAll(right);
+        return result;
+    }
+
+}
