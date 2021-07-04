501e26b0ce03ea3f7f9fda29538ab127be9d2479
mostafa <me.cssign@gmail.com>
2021-07-04 15:15:06 +0200

    Implement PostOrderTraversal

---
+++ src/main/java/org/mostafaism/problemsolving/TreesAndGraphs/Extras/PostOrderTraversal.java
@@ -0,0 +1,27 @@
+package org.mostafaism.problemsolving.TreesAndGraphs.Extras;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.mostafaism.problemsolving.datastructure.Tree.BinaryTreeNode;
+
+public class PostOrderTraversal<E> {
+
+    public List<E> postOrderTraversal(BinaryTreeNode<E> root) {
+        // Base case.
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        // Recursive step.
+        List<E> left = postOrderTraversal(root.left());
+        List<E> right = postOrderTraversal(root.right());
+        List<E> result = new ArrayList<>();
+        result.addAll(left);
+        result.addAll(right);
+        result.add(root.data());
+        return result;
+    }
+
+}
