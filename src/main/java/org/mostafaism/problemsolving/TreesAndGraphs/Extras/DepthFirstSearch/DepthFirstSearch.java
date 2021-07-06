58fac450305872248046b14b7c0598b3a18674e5
mostafa <me.cssign@gmail.com>
2021-07-07 01:03:46 +0200

    Define a DepthFirstSearch interface

---
+++ src/main/java/org/mostafaism/problemsolving/TreesAndGraphs/Extras/DepthFirstSearch/DepthFirstSearch.java
@@ -0,0 +1,18 @@
+package org.mostafaism.problemsolving.TreesAndGraphs.Extras.DepthFirstSearch;
+
+import java.util.Optional;
+
+import org.mostafaism.problemsolving.datastructure.Graph.GraphNode;
+
+public interface DepthFirstSearch<E> {
+    /**
+     * Returns an Optional of a GraphNode with a value equal to the specified value
+     * if the value exists, otherwise Optional.empty.
+     *
+     * @param startNode a node from which to start searching
+     * @param value     the value to search for
+     * @return an Optional of a GraphNode with a value equal to the specified value
+     *         if the value exists, otherwise Optional.empty
+     */
+    Optional<GraphNode<E>> search(GraphNode<E> startNode, E value);
+}
