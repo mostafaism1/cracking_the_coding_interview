a82dd8e42d4679da81be35f1968b02a2db43ffbc
mostafa <me.cssign@gmail.com>
2021-07-07 02:05:19 +0200

    Add tests for BreadthFirstSearch

---
+++ src/test/java/org/mostafaism/problemsolving/TreesAndGraphs/Extras/Search/BreadthFirstSearchTest.java
@@ -0,0 +1,12 @@
+package org.mostafaism.problemsolving.TreesAndGraphs.Extras.Search;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class BreadthFirstSearchTest extends SearchTest {
+
+    @BeforeEach
+    void setup() {
+        search = new BreadthFirstSearch<>();
+    }
+
+}
