09935c523a8da6d1862283e67cd1c951cde49577
mostafa <me.cssign@gmail.com>
2021-07-26 05:43:48 +0200

    Add BinaryTreeNode.isLeaf method

--- src/main/java/org/mostafaism/problemsolving/datastructure/Tree/BinaryTreeNode.java
+++ src/main/java/org/mostafaism/problemsolving/datastructure/Tree/BinaryTreeNode.java
@@ -2,4 +2,8 @@ package org.mostafaism.problemsolving.datastructure.Tree;
 
 public record BinaryTreeNode<E> (E data, BinaryTreeNode<E> left, BinaryTreeNode<E> right) {
 
+    public boolean isLeaf() {
+        return left == null && right == null;
+    }
+
 }
